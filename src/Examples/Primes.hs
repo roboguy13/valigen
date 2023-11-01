5781aa2ed44d5d259ab444b0b8f3726afb2ffab2
David Young <roboguy06@gmail.com>
2023-11-01 11:15:01 -0500

    Another example

--- src/Examples/Primes.hs
+++ src/Examples/Primes.hs
@@ -3,6 +3,7 @@ module Examples.Primes where
 import Test.QuickCheck
 import ValiGen.ValiGen
 import GHC.Data.BooleanFormula (BooleanFormula)
+import Control.Monad (replicateM)
 
 newtype Prime = Prime { getPrime :: Int }
 
@@ -16,8 +17,46 @@ example1Gen :: Gen Int
 example1Gen = uncurry toGen example1
 -- example1 = Filter (Choose (1, 1000)) (Check isPrime)
 
--- example1 :: ValiGen v Int
--- example1 = Choose (1, 1000) `Intersect` isPrime
+newtype EvenList = EvenList [Int]
+  deriving (Show, Ord, Eq)
+
+-- instance Arbitrary EvenList where
+--   arbitrary = example2
+
+example2 :: Gen EvenList
+example2 = fmap EvenList $ (`suchThat` ((> 50) . length)) $ resize 100 $ listOf (choose (1, 20))
+
+example2' :: Gen EvenList
+example2' = fmap EvenList $ (`suchThat` ((> 50) . length)) $ do
+  n <- getSize
+  sz <- choose (0, n)
+  vectorOf sz (choose (1, 20))
+
+example2'' :: Gen EvenList
+example2'' = fmap EvenList $ do
+  n <- getSize
+  sz <- choose (0, n)
+  vectorOf sz (choose (1, 20)) `suchThat` ((> 50) . length)
+
+example2''' :: Gen EvenList
+example2''' = fmap EvenList $ do
+  n <- getSize
+  sz <- choose (0, n) -- TODO: Would like to somehow combine the `suchThat` with this line...
+  replicateM sz (choose (1, 20)) `suchThat` ((> 50) . length)
+
+-- choose (0, n) >>= \sz -> f sz `suchThat` ((> 50) . length)
+-- choose (0, n) >>= \sz -> With (sz .> 50) (f sz)
+--   ^ Needs to run the bind continuation with a dummy variable first, to get constraints?
+
+example2_ :: Gen EvenList
+example2_ = fmap EvenList $ do
+  xs <- resize 100 $ listOf (choose (1, 20))
+  if length xs > 50
+    then pure xs
+    else discard
+
+easy :: EvenList -> Property
+easy (EvenList xs) = even (length xs) ==> True
 
 -- example2 :: ValiGen v Int
 -- example2 = (isPrime `Union` isEven) `Implies` \_ ->
@@ -33,7 +72,3 @@ isPrime :: Int -> Bool
 isPrime x = go [2 .. floor (sqrt @Double (fromIntegral x))]
   where
     go = all ((> 0) . (x `mod`))
-
--- -- isPrime :: ValiGen v Int
--- -- isPrime = Implies (Choose (2, 1000)) $ \x ->
--- --   Check undefined
